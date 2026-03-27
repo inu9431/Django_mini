7bc2ea59f07082c47551e90ffa2dd47093ae86a3
inu9431 <>
2026-03-27 17:51:06 +0900

    chore: Serve media files in development
    - 개발 환경에서 업로드된 이미지를 URL로 접근할 수 있도록 static() 설정 추가

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('app.users.urls')),
@@ -13,6 +14,8 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG and not settings.TESTING:
     import debug_toolbar
