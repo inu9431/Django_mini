a76742d8119d98c33ac24cb54e26eb08079e915c
inu9431 <>
2026-03-27 16:25:34 +0900

    test1

--- app/budgets/views.py
+++ app/budgets/views.py
@@ -7,29 +7,52 @@ from drf_spectacular.utils import extend_schema
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics
 
+
 class AccountListView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
+
     @extend_schema(responses={200: AccountSerializer(many=True)})
     def get(self, request):
         accounts = Account.objects.filter(user=request.user)
         serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data)
-    @extend_schema(request= AccountSerializer, responses={201: AccountSerializer})
+
+    @extend_schema(request=AccountSerializer, responses={201: AccountSerializer})
     def post(self, request):
         serializer = AccountSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
 class AccountDetailView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
-    @extend_schema(responses={200: None})
+
+    @extend_schema(responses={200: AccountSerializer})
+    def get(self, request, pk):
+        account = Account.objects.filter(user=request.user, pk=pk).first()
+        if not account:
+            return Response({"error": "계좌를 찾을수 없습니다"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = AccountSerializer(account)
+        return Response(serializer.data)
+
+    @extend_schema(request=AccountSerializer, responses={200: AccountSerializer})
+    def put(self, request, pk):
+        account = Account.objects.filter(user=request.user, pk=pk).first()
+        if not account:
+            return Response({"error": "계좌를 찾을수 없습니다"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = AccountSerializer(account, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    @extend_schema(responses={204: None})
     def delete(self, request, pk):
         account = Account.objects.filter(user=request.user, pk=pk).first()
         if not account:
-            return Response({"error": "계좌를 찾을수 없습니다"},status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "계좌를 찾을수 없습니다"}, status=status.HTTP_404_NOT_FOUND)
         account.delete()
-        return Response({"message": "삭제 완료"},status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class TransactionListView(generics.ListCreateAPIView):
@@ -48,6 +71,7 @@ class TransactionListView(generics.ListCreateAPIView):
             from rest_framework.exceptions import PermissionDenied
             raise PermissionDenied("본인 계좌에만 거래내역을 추가할수 있습니다")
         serializer.save()
+
 
 class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TransactionSerializer
